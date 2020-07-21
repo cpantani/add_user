6816348a1d229504d5770c81e108d54f3df86d6c
cpantani <cpantani29@gmail.com>
2020-07-20 20:44:59 -0400

    edits to code, no run errors, Form now works, no contact to backend

--- src/AddUserApp.js
+++ src/AddUserApp.js
@@ -1,30 +1,27 @@
 import React, { Component } from "react";
-
 import Form from './AddUserForm';
-
-
 
 class AddUser extends Component{
 
-
-	state= {
-		fields:{},
+	state={
+		fields: {}
 	};
 
-	onSubmit = (fields) =>{
-		this.setState({ fields });
-		console.log('App comp got: ', fields);
+	onSubmit= fields => {
+		this.setState({ fields })
 	};
 
-	render() {
-		return(
+	render(){
+	return (
 
 		<div className="App">
-			<Form onSubmit={fields => this.onSubmit(fields)}/>
-			<p>{JSON.stringify(this.state.fields,null,2)}</p>
+			<Form onSubmit={fields => this.onSubmit(fields)} />
+			<p>
+				{JSON.stringify(this.state.fields,null,2)}
+			</p>
 		</div>
 
-		);
+	);
 	}
 }
 
--- src/AddUserForm.js
+++ src/AddUserForm.js
@@ -1,70 +1,68 @@
-import React from 'react';
+import React, { Component } from "react";
 
 export default class Form extends React.Component {
-	state= {
-		firstName:'',
-		lastName:'',
-		username:'',
-		email:'',
-		password:'',
-	};
+		state= {
+			firstName:'',
+			lastName:'',
+			username:'',
+			email:'',
+			password:'',};
 
-	change = e =>{
+
+	change = (e) => {
 		this.setState({
 			[e.target.name]: e.target.value
-		});
-	};
+		})
+	}
 
 	onSubmit = (e) => {
 		e.preventDefault();
 		this.props.onSubmit(this.state)
-		console.log(this.state);
+		this.setState({
+			firstName:'',
+			lastName:'',
+			username:'',
+			email:'',
+			password:'',})
 	}
 
 
 	render() {
-		return(
-		<form>
-			<input name = "first name"
-				   placeholder = "first name"
-				   value ={this.state.firstName}
-				   onChange={e => this.change(e)}
-			/>
-			<br />
-			<input name = "last name"
-				   placeholder = "last name"
-				   value ={this.state.firstName}
-				   onChange={e => this.change(e)}
-			/>
-			<br />
-			<input name = "last name"
-				   placeholder = "last name"
-				   value ={this.state.firstName}
-				   onChange={e => this.change(e)}
-			/>
-			<br />
-			<input name = "username"
-				   placeholder = "username"
-				   value ={this.state.firstName}
-				   onChange={e => this.change(e)}
-			/>
-			<br />
-			<input name = "email"
-				   placeholder = "email"
-				   value ={this.state.firstName}
-				   onChange={e => this.change(e)}
-			/>
-			<br />
-			<input name = "password"
-				   type = 'password'
-				   placeholder = "password"
-				   value ={this.state.firstName}
-				   onChange={e => this.change(e)}
-			/>
-			<br />
-			<button onClick={e => this.onSubmit(e)}>Submit</button>
-		</form>
-		);
-	}
+		return (
+			<form>
+				<input
+					name="firstName"
+					placeholder="first name"
+					value={this.state.firstName}
+					onChange={e => this.change(e)}
+				/>
+				<input
+					name="lastName"
+					placeholder="lastName"
+					value={this.state.lastName}
+					onChange={e => this.change(e)}
+				/>
+				<input
+					name="username"
+					placeholder="username"
+					value={this.state.username}
+					onChange={e => this.change(e)}
+				/>
+					<input
+					name="email"
+					placeholder="email"
+					value={this.state.email}
+					onChange={e => this.change(e)}
+				/>
+					<input
+					name="password"
+					placeholder="password"
+					value={this.state.password}
+					onChange={e => this.change(e)}
+				/>
 
+				<button onClick={e => this.onSubmit(e)}> Submit </button>
+			</form>
+		)
+	}
 }
